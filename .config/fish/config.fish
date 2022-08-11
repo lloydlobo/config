828ed6279560654cf333aad217f3927d6455cfb1
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-11 15:16:35 +0530

    feat(fish): add config.fish & fish_variables

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # navi widget fish | source
+    
+    alias config='/usr/bin/git --git-dir=/home/ll/.cfg/ --work-tree=/home/ll'
+
+    # similar to neofetch --> display system info
+    macchina
+end
