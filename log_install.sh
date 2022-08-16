f97a5237fffbb90fa4f06b93b6d9fca125f55884
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-16 17:44:34 +0530

    feat(install): init install log to keep track
    - draft to keep track of all installs
    - filter it later into an `install.sh` bash script

---
+++ log_install.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+gh auth login
+
+#
+# /config DOTFILES BARE REPO
+#
+# Import dotfiles from github for .config
+echo ".cfg" >> .gitignore
+git clone --bare git@github.com:lloydlobo/config.git $HOME/.cfg/
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+
+#
+# RUSTUP | CARGO 
+#
+# Setup rustup cargo & update it
+rustup default stable
+rustup update
+
+
+#
+# MUSIC | MPD | MPV
+#
+
+# Prerequisite mpc mpd ncmpcpp
+sudo pacman -S timidity
+
+#
+# YAY
+#
+# Install yay 
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Setup yay
+yay -Y --gendb
+yay -Syu devel
+yay -Y --devel --save
+
+#
+# i3 - PLUGINS
+#
+yay autotiling
+yay -S i3-resurrect
+yay xorg-xbacklight
+yay light
+yay bat
+yay thefuck
+
+#
+# ASTRONVIM
+#
+yay astronvim
+yay lazygit
+# lf aur/lf Ranger in Go-lang - Option 98 in yay.,. first in aur
+yay lf
+yay ripgrep
+
+# OTHER YAY
+yay nerd-fonts
+yay macchina-bin
+yay procs
+yay hyperfine
+
+# Shell Prompt
+yay starship
+
+# https://wiki.archlinux.org/title/Dotfiles
+# Dotfiles bare config alias
+yay bash-complete-alias
+
+# Navi & dependencies
+yay navi # man-page alternative in Rust
+yay fzf
+yay skim
+
+# starship setup
+starship init bash
+starship init fish
+starship init zsh
+mkdir ~/.config/starship/ && touch ~/.config/starship/starship.toml
+export STARSHIP_CONFIG=~/.config/starship/starship.toml
+
+yay mcfly
+echo "source /usr/share/doc/mcfly/mcfly.bash" >> .bashrc
+
+# wallppaper theme
+yay python-pywal
+
+
+#
+# PARU
+#
+# Install paru, like yay
+yay paru
+
+# wallpapers
+paru -S nordic-wallpapers
+
+
