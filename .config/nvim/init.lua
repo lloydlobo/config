cc944f6b6749d520a84962240dbd2de77c9a99df
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-17 21:50:57 +0530

    feat(neovim): use starter config for plugin packer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,42 +1,43 @@
 -- https://github.com/krady21/dotfiles/blob/master/.config/nvim/init.vim
 -- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
  
---
 -- Install packer
---
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	is_bootstrap = true
-	vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim' .. install_path)
-	vim.cmd [[packadd packer.nvim]]
+  is_bootstrap = true
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
 end
 
 -- stylua: ignore start
 require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'tpope/vim-fugitiv'
-	use 'nvim-treesitter/nvim-treesitter'
-	use 'nvim-treesitter/nvim-treesitter-textobjects'
-	use 'neovim/nvim-lspconfig'
-	use 'williamboman/nvim-lsp-installer'
-	use 'mjbach/onedark.nvim'
-	use 'nvim-lualine/lualine.nvim'
-	use 'lukas-reineke/indent-blankline.nvim'
-	use 'tpope/vim-sleuth'
-	use { 'nvim-telescope/telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use 'wbthomason/packer.nvim'                                                    -- Package manager
+  use 'tpope/vim-fugitive'                                                        -- Git commands in nvim
+  use 'tpope/vim-rhubarb'                                                         -- Fugitive-companion to interact with github
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }       -- Add git related info in the signs columns and popups
+  use 'numToStr/Comment.nvim'                                                     -- "gc" to comment visual regions/lines
+  use 'nvim-treesitter/nvim-treesitter'                                           -- Highlight, edit, and navigate code
+  use 'nvim-treesitter/nvim-treesitter-textobjects'                               -- Additional textobjects for treesitter
+  use 'neovim/nvim-lspconfig'                                                     -- Collection of configurations for built-in LSP client
+  use 'williamboman/nvim-lsp-installer'                                           -- Automatically install language servers to stdpath
+  use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } }               -- Autocompletion
+  use { 'L3MON4D3/LuaSnip', requires = { 'saadparwaiz1/cmp_luasnip' } }           -- Snippet Engine and Snippet Expansion
+  use 'mjlbach/onedark.nvim'                                                      -- Theme inspired by Atom
+  use 'nvim-lualine/lualine.nvim'                                                 -- Fancier statusline
+  use 'lukas-reineke/indent-blankline.nvim'                                       -- Add indentation guides even on blank lines
+  use 'tpope/vim-sleuth'                                                          -- Detect tabstop and shiftwidth automatically
+  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } } -- Fuzzy Finder (files, lsp, etc)
 
-	-- Fuzzy Finder Algorithm requires local dependencies
-	-- Load if "make" is available
-	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fnexecutable "make" == 1 }
-	
-	if is_bootstrap then
-		require('packer').sync()
-	end
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable "make" == 1 }
+
+  if is_bootstrap then
+    require('packer').sync()
+  end
 end)
 -- stylua: ignore end
 
---
 -- When we are bootstrapping a configuration, it doesn't
 -- make sense to execute the rest of the init.lua.
 --
@@ -49,3 +50,15 @@ if is_bootstrap then
   print '=================================='
   return
 end
+
+-- Automatically source and re-compile packer whenever you save this init.lua
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = vim.fn.expand '$MYVIMRC',
+})
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
