6fe86f811c7465f8edf1717c2253ac1b8f620cc5
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-18 04:48:26 +0530

    feat(neovim): add keymappings & leader key

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -95,3 +95,26 @@ vim.cmd [[colorscheme onedark]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
+
+-- [[ Basic Keymaps ]]
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Visual
+-- Stay in indent mode
+-- TODO
+-- Move text up and down
+--keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+--keymap("v", "<A-j>", ":m .-2<CR>==", opts)
+--keymap("v", "p", "= dP", opts)               --  yank and paste once ;chris@machine 25 custom keymaps set with lua
