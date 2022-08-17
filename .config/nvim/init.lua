ba84369c123a831064165105708035b8527976af
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-17 22:11:30 +0530

    feat(neovim): set gui case history theme etc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -69,4 +69,29 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 -- Set highlight on search
 vim.o.hlsearch = false
 
+-- Make line numbers default
+vim.wo.number = true
 
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching unless /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+-- Set colorscheme
+vim.o.termguicolors = true
+vim.cmd [[colorscheme onedark]]
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
