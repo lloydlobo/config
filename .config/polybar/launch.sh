4d3276d87a40ab45a8dadf6f11df2eb9d4842287
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-17 15:20:41 +0530

    refactor(polybar): break out config into master & modules

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,7 +3,6 @@
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
-
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
@@ -11,9 +10,14 @@ polybar-msg cmd quit
 echo "---" | tee -a /tmp/polybar1.log
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar i3-bar 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 echo "Bars launched"
 
-
-
+# https://gitlab.com/dwt1/dotfiles/-/blob/master/.config/polybar/launch.sh
+# # Wait until the processes have been shut down
+# while pgrep -u $UID -x polybar >/dev/null; do : ; done
+# for m in $(polybar --list-monitors | cut -d":" -f1); do
+# 	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
+# done
+# echo "Bars launched..."
