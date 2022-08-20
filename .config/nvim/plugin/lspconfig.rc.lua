aa763db35e704c8b34217c13bc0ed760a14ea931
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-20 08:49:48 +0530

    feat(neovim): add tsserver lspconfig plugin

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -13,3 +13,11 @@ local on_attatch = function(client, buffer)
     vim.apt.nvim_command [[augroup end]]
   end
 end
+
+-- Config - TypeScript
+nvim_lsp.tsserver.setup {
+  on_attach = on_attach,
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" },
+  capabilities = capabilities
+}
