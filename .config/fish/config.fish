189824277e12f7cbb14016eef9db9da16d2b3c26
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-21 20:19:46 +0530

    style(kitty): add jetbrains bold and italic fonts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,6 +16,13 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 zoxide init fish | source
 
 # Credits: https://github.com/craftzdog/dotfiles-public/blob/master/.config/fish/config.fish
+# theme
+#set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
 #
 # aliases
 #
