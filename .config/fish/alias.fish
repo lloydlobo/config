4a9af729876a9aa50a969a9a83cdb4675ad4076f
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-09-03 15:14:52 +0530

    chore(fish): disable alias of cd to zoxide z

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -5,6 +5,10 @@ alias ls=lsd
 command -qv nvim && alias vim nvim
 set -gx EDITOR nvim
 
+# cd replacement with zoxide `z`
+# alias cd "z"
+# alias zz "z -" -- toggle back and forth
+
 # ls
 alias ls "ls -p -G" # -p, --indicator-style=slash -G, --no-group
 alias la "ls -A" # -A, --almost-all
@@ -19,20 +23,20 @@ alias tl='tmux list-sessions'
 
 # https://github.com/matchai/dotfiles/blob/b6c6a701d0af8d145a8370288c00bb9f0648b5c2/.config/fish/alias.fish
 # git
-abbr -a gs  git status -sb
-abbr -a ga  git add
-abbr -a gc  git commit
+abbr -a gs git status -sb
+abbr -a ga git add
+abbr -a gc git commit
 abbr -a gcm git commit -m
 abbr -a gca git commit --amend
 abbr -a gcl git clone
 abbr -a gco git checkout
-abbr -a gp  git push
+abbr -a gp git push
 abbr -a gpl git pull
-abbr -a gl  git l
-abbr -a gd  git diff
+abbr -a gl git l
+abbr -a gd git diff
 abbr -a gds git diff --staged
-abbr -a gr  git rebase -i HEAD~15
-abbr -a gf  git fetch
+abbr -a gr git rebase -i HEAD~15
+abbr -a gf git fetch
 abbr -a gfc git findcommit
 abbr -a gfm git findmessage
 abbr -a gco git checkout
@@ -41,14 +45,21 @@ abbr -a gco git checkout
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 # config dotfiles
-abbr -a ds  config status -s -b
-abbr -a da  config add
-abbr -a dc  config commit
+abbr -a ds config status -s -b
+abbr -a da config add
+abbr -a dc config commit
 abbr -a dcm config commit -m
-abbr -a dp  config push
-abbr -a dl  config l
-abbr -a dd  config diff
+abbr -a dp config push
+abbr -a dl config l
+abbr -a dd config diff
 abbr -a dds config diff --staged
 
+#
 # misc
+#
+
+# reload fish config
 alias reload="exec fish"
+
+# Display all fonts
+alias fcl="fc-list : family spacing outline scalable | grep -e spacing=100 -e spacing=90 | grep -e outline=True | grep -e scalable=True"
