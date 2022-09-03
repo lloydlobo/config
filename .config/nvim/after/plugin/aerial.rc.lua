0a7e2bc6eca27e4d91e6e0247a5b99626c456fd7
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-09-03 15:52:02 +0530

    feat(neovim): add arial symbols & descriptions

--- .config/nvim/after/plugin/aerial.rc.lua
+++ .config/nvim/after/plugin/aerial.rc.lua
@@ -4,14 +4,50 @@ if not status_ok then
 end
 
 aerial.setup({
+	close_behavior = "global",
+	backends = { "lsp", "treesitter", "markdown" },
+	min_width = 28,
+	show_guides = true,
+	filter_kind = false,
+	guides = {
+		mid_item = "├ ",
+		last_item = "└ ",
+		nested_top = "│ ",
+		whitespace = "  ",
+	},
+
 	on_attach = function(bufnr)
 		-- Toggle the aerial window with <leader>a
 		vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>a", "<cmd>AerialToggle!<CR>", {})
 		-- Jump forwards/backwards with '{' and '}'
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "{", "<cmd>AerialPrev<CR>", {})
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "}", "<cmd>AerialNext<CR>", {})
+		vim.api.nvim_buf_set_keymap(
+			bufnr,
+			"n",
+			"{",
+			"<cmd>AerialPrev<CR>",
+			{ desc = "Jump backwards in Aerial" }
+		)
+		vim.api.nvim_buf_set_keymap(
+			bufnr,
+			"n",
+			"}",
+			"<cmd>AerialNext<CR>",
+			{ desc = "Jump forwards in Aerial" }
+		)
 		-- Jump up the tree with '[[' or ']]'
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "[[", "<cmd>AerialPrevUp<CR>", {})
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "]]", "<cmd>AerialNextUp<CR>", {})
+		vim.api.nvim_buf_set_keymap(
+			bufnr,
+			"n",
+			"[[",
+			"<cmd>AerialPrevUp<CR>",
+			{ desc = "Jump up and backwards in Aerial" }
+		)
+		vim.api.nvim_buf_set_keymap(
+			bufnr,
+			"n",
+			"]]",
+			"<cmd>AerialNextUp<CR>",
+			{ desc = "Jump up and forwards in Aerial" }
+		)
 	end,
 })
