bdbab5dad2604ede2e02b66fcd6ac8a2c8006710
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-09-04 15:46:59 +0530

    feat(neovim): add eof tilde msgsep fillchars EndBuffer

--- .config/nvim/lua/core/highlights.lua
+++ .config/nvim/lua/core/highlights.lua
@@ -9,7 +9,8 @@ vim.opt.pumblend = 5
 vim.opt.background = "dark"
 --[[ For NeoVim, you can set the fillchars value for eob to a space character and that will effectively hide it. (This might not work for plain Vim).
 https://stackoverflow.com/questions/3813059/is-it-possible-to-not-display-a-for-blank-lines-in-vim-neovim ]]
-vim.cmd([[set fillchars=eob:\ ,fold:\ ,vert:\│]]) -- QUICKFIX Hide tilde squigly line at end of file -- https://stackoverflow.com/a/70887809
+-- vim.cmd([[set fillchars=eob:\ ,fold:\ ,vert:\│]]) -- QUICKFIX Hide tilde squigly line at end of file -- https://stackoverflow.com/a/64619522
+vim.cmd([[set fillchars=fold:\ ,vert:\│,eob:\ ,msgsep:‾]]) -- QUICKFIX Hide tilde squigly line at end of file -- https://stackoverflow.com/a/64619522
 
 -- https://github.com/RishabhRD/rdconfig/blob/master/.config/nvim/lua/rd/ts_config.lua
 -- vim.cmd [[highlight IncludedC guibg=#373b41]]
